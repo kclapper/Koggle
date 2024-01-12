21fff5a6515ee21e36f01b0a38e37a94987b3e0c
Kyle Clapper <35474281+kclapper@users.noreply.github.com>
2024-01-12 18:53:01 -0500

    Test config works

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,24 @@
 import {describe, expect, test} from '@jest/globals';
 
-describe('sum module', () => {
+describe('Common Boggle Tests', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3);
+  });
+});
+
+describe('Test 4x4 Boggle', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3);
+  });
+});
+
+describe('Test 5x5 Boggle', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3);
+  });
+});
+
+describe('Test Solver', () => {
   test('adds 1 + 2 to equal 3', () => {
     expect(1 + 2).toBe(3);
   });
